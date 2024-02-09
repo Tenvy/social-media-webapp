d06ce04da22a7251b08b0aaf8d88232e2d520e97
tenvy <riskypabji@gmail.com>
2024-02-09 23:50:59 +0800

    feat: Logo For Sidebar Component

---
+++ src/components/ui/sidebar/logo.tsx
@@ -0,0 +1,9 @@
+const Logo = async () => {
+    return (
+        <div className='mx-2 text-primary-color font-bold text-3xl text-center pb-6'>
+            Socmed-App
+        </div>
+    )
+}
+
+export default Logo
