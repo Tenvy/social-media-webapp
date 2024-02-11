1cb9ec2f0c935445d3bffde872389f606ecb5603
tenvy <riskypabji@gmail.com>
2024-02-12 00:07:06 +0800

    feat: GET Request for album route

--- src/app/api/album/route.ts
+++ src/app/api/album/route.ts
@@ -1,23 +1,39 @@
 import { authOptions } from "@/lib/AuthOptions";
-import prisma from "@/utils/db"
+import prisma from "@/utils/db";
 import { getServerSession } from "next-auth";
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-    const { NamaAlbum, Deskripsi } = await request.json()
-    try {
-        const session: any = await getServerSession(authOptions);  
-        if (!session) return NextResponse.json({msg: 'Kamu belum login'})
-        const response = await prisma.album.create({
-            data: {
-                NamaAlbum,
-                Deskripsi,
-                TanggalDibuat: new Date,
-                UserID : session?.user?.UserID,
-            }
-        })
-        return NextResponse.json(response)
-    } catch (error) {
-        return NextResponse.json({msg: "Something went wrong.", error})
-    }
+  const { NamaAlbum, Deskripsi } = await request.json();
+  try {
+    const session: any = await getServerSession(authOptions);
+    if (!session) return NextResponse.json({ msg: "Kamu belum login" });
+    const response = await prisma.album.create({
+      data: {
+        NamaAlbum,
+        Deskripsi,
+        TanggalDibuat: new Date(),
+        UserID: session?.user?.UserID,
+      },
+    });
+    return NextResponse.json(response);
+  } catch (error) {
+    return NextResponse.json({ msg: "Something went wrong.", error });
+  }
+}
+
+export async function GET(req: Request) {
+  try {
+    const session: any = await getServerSession(authOptions);
+
+    const response = await prisma.album.findMany({
+      where: {
+        UserID: session?.user?.UserID,
+      },
+    });
+    return NextResponse.json(response);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return NextResponse.json({ msg: "Something went wrong.", error });
+  }
 }
