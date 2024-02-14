ab2c8a01bdc044091dfa09f64dc77d629571accb
tenvy <riskypabji@gmail.com>
2024-02-14 23:53:26 +0800

    chore: add fotoID and user to fotoType

--- src/type/foto.ts
+++ src/type/foto.ts
@@ -1,8 +1,12 @@
+import { userType } from "./user";
+
 export type fotoType = {
+    FotoID: number;
     JudulFoto: string;
     DeskripsiFoto: string;
     TanggalUnggah: Date;
     LokasiFile: string;
     AlbumID: number
     UserID: number;
+    user?: userType;
 }
