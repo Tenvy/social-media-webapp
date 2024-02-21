7bc5f37c2744ced7f589c13c7a9171c3024d662c
tenvy <riskypabji@gmail.com>
2024-02-22 00:33:34 +0800

    feat: protocol for supabase image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        disableStaticImages: false,
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: `${process.env.SUPABASE_PROJECT_URL_WITHOUT_HTTPS}`,
+          },
+        ],
+      },
+};
 
 export default nextConfig;
