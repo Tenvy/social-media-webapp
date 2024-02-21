db5c86c89b506b02157bec7d7fdce1e8cde8080e
tenvy <riskypabji@gmail.com>
2024-02-22 00:29:36 +0800

    chore: removed session check

--- src/services/register.ts
+++ src/services/register.ts
@@ -1,18 +1,13 @@
 import { userType } from '@/type/user';
-import { signIn, signOut, useSession } from 'next-auth/react';
+import { signIn } from 'next-auth/react';
 
 const CreateUser = async (data: userType) => {
-  const { data: session } = useSession();
   const response = await fetch('/api/user', {
     method: 'POST',
     cache: 'no-store',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data),
   });
-
-  if(session) {
-    signOut()
-  }
 
   if(!response) return null
 
