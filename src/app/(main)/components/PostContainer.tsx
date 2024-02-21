2e0a0d1e6d2c6f8c69b54800d5a90d62165f0fb0
tenvy <riskypabji@gmail.com>
2024-02-22 00:32:12 +0800

    feat: add Post Component

--- src/app/(main)/components/PostContainer.tsx
+++ src/app/(main)/components/PostContainer.tsx
@@ -4,28 +4,16 @@ import { fotoType } from '@/type/foto'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import Post from './Post'
 
 const PostContainer = async () => {
     const data = await getFoto()
-
+    console.log(data)
     return (
         <div className='grid grid-cols-3 gap-1'>
-            {data ? (data.map((res: fotoType, key: number) => (
-                <Link key={key} href={`/post/${res.FotoID}`}>
-                    <div className='w-full aspect-square hover:opacity-90'>
-                        <Image src={res.LokasiFile} alt={res.JudulFoto} width={400} height={400} className='w-full h-full object-cover' />
-                    </div>
-                </Link>
-            ))) : (
-                <Skeleton className="w-[400px] h-[400px] rounded-full" />
-            )}
-            {/* {data.map((res: fotoType, key: number) => (
-                <Link key={key} href={`/post/${res.FotoID}`}>
-                    <div className='w-full aspect-w-1 aspect-square hover:opacity-90'>
-                        <Image src={res.LokasiFile} alt={res.JudulFoto} width={400} height={400} className='w-full h-full object-cover' />
-                    </div>
-                </Link>
-            ))} */}
+            {data.map((res: fotoType, key: number) => (
+                    <Post {...res} key={key} />
+            ))}
         </div>
     )
 }
