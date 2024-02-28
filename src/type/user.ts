36182b072c68c526f1100096e9c0d457c1cb15d6
tenvy <riskypabji@gmail.com>
2024-02-28 12:20:21 +0800

    chore: add album and foto to user type

--- src/type/user.ts
+++ src/type/user.ts
@@ -1,7 +1,11 @@
+import { album, foto } from "@prisma/client";
+
 export type userType = {
     Username: string;
     Password: string;
     Email: string;
     NamaLengkap: string;
     Alamat: string;
+    album: album[];
+    foto: foto[];
 }
