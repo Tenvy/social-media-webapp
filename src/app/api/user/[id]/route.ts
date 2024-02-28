40d95d3fc717c703aa20da7a118ef8bb54062f00
tenvy <riskypabji@gmail.com>
2024-02-28 12:22:35 +0800

    chore: GET request to user route by id

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -4,16 +4,19 @@ import { NextResponse } from "next/server";
 export async function GET(req: Request, context: { params: { id: string } } ) {
     const id = context.params.id
     try {
-        const parsedId = parseInt(id, 10);
-
-        if (isNaN(parsedId)) {
-            return NextResponse.json({ msg: 'Invalid ID provided.' });
-        }
-        
         if(id) {
             const response = await prisma.user.findUnique({
                 where: {
-                    UserID: parsedId
+                    Username: id
+                },
+                select: {
+                    UserID: true,
+                    Username: true,
+                    Email: true,
+                    NamaLengkap: true,
+                    Alamat: true,
+                    album: true,
+                    foto: true
                 }
             })
             return NextResponse.json(response)
